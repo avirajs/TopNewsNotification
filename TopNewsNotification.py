d8d5fbe9629af05a3aa0c6a272e975f86b1754c6
Aviraj Sinha <avirajs@smu.edu>
2017-07-25 17:56:56 -0500

    Working on collecting useful news keywords

--- TopNewsNotification.py
+++ TopNewsNotification.py
@@ -10,11 +10,12 @@ def econ():
         # BeautifulSoup objects can be sorted through easy
         soup = BeautifulSoup(plain_text, 'html.parser')
         # print(soup.get_text())
+        temp=list()
         for link in soup.findAll("a", {"itemprop": "url"}):  # {'class': 's-result-item celwidget '}
             href = link.get("href")
-            print('https://www.economist.com'+href)
+            temp.append(str('https://www.economist.com'+href ))
 
-        return soup.findAll("a", {"itemprop": "url"})
+        return temp[80:]
 
 def nyt():
     url = 'https://www.nytimes.com'
@@ -25,6 +26,7 @@ def nyt():
     soup = BeautifulSoup(plain_text, 'html.parser')
 
     # print(soup.get_text())
+    temp = list
     for link in soup.findAll("h2", {"class": "story-heading"}):  # {'class': 's-result-item celwidget '}
         href = link.get("a")
         #     title = link.string  # just the text, not the HTML
@@ -32,9 +34,10 @@ def nyt():
         # continue
         # print(href)
         link=str(link)
-        print(link[link.find("href")+5:])
+
+        temp+=(link[link.find("href")+5:])
     # #get_single_item_data(href)
-    return soup.findAll("h2", {"class": "story-heading"})
+    return temp
 
 def wsj():
     url = 'https://www.wsj.com/'
@@ -44,7 +47,7 @@ def wsj():
     # BeautifulSoup objects can be sorted through easy
     soup = BeautifulSoup(plain_text,'html.parser')
 
-
+    temp = list
     #print(soup.get_text())
     for link in soup.findAll("a", {"class": "wsj-headline-link"}):#{'class': 's-result-item celwidget '}
         href = link.get("a")
@@ -53,8 +56,9 @@ def wsj():
             #continue
        # print(href)
         link = str(link)
-        print(link[link.find("href") + 5:])
-    return soup.findAll("a", {"class": "wsj-headline-link"})
+
+        temp+=(link[link.find("href") + 5:])
+    return temp
 
 def bbc():
     url = 'http://www.bbc.com/news'
@@ -63,27 +67,36 @@ def bbc():
     plain_text = source_code.text.encode('ascii', 'replace')
     # BeautifulSoup objects can be sorted through easy
     soup = BeautifulSoup(plain_text, 'html.parser')
-
+    temp = list;
     # print(soup.get_text())
     for link in soup.findAll("a",{'class': 'gs-c-promo-heading nw-o-link-split__anchor gs-o-faux-block-link__overlay-link gel-pica-bold'}):  # {'class': 's-result-item celwidget '}
         link = str(link)
-        if "http" in link:
-            print(link[link.find("href") + 5:])
-        else:
-            print('http://www.bbc.com/news' + link[link.find("href") + 6:])
 
-    return soup.findAll("a",{'class': 'gs-c-promo-heading nw-o-link-split__anchor gs-o-faux-block-link__overlay-link gel-pica-bold'});
+        if "http" in link:
+            temp+=link[link.find("href") + 5:]
+        else:
+            temp +=('http://www.bbc.com/news' + link[link.find("href") + 6:])
+
+    return temp
 
 #adding new
-allNewsRaw = econ()+nyt()+wsj()+bbc()
-allNews=str(allNewsRaw)
+allNews=econ();
+
+
+import re
+
+for i in allNews
+print(re.split(r'[/-]+', allNews[0]))
+
+
+
 
 #print(allNe ws)
 from nltk.corpus import words
 word_list = words.words()
 
-import re, collections
-w=["Syria","Trump","Mccain"]
-pattern = re.compile("|".join(w), flags = re.IGNORECASE)
-print(collections.Counter(pattern.findall(allNews)))
+# import re, collections
+# w=["Syria","Trump","Mccain"]
+# pattern = re.compile("|".join(w), flags = re.IGNORECASE)
+# print(collections.Counter(pattern.findall(allNews)))
 
